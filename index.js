cdf3c69beb1d5197633367ec348f884d29160b32
Julian Gruber <julian@juliangruber.com>
2017-05-01 14:23:51 +0200

    refactor to use microcomponent

--- index.js
+++ index.js
@@ -1,60 +1,34 @@
 'use strict'
 
 const html = require('bel')
-const Component = require('nanocomponent')
-const inherits = require('util').inherits
-const morph = require('nanomorph')
+const component = require('microcomponent')
 
-module.exports = Ruler
-inherits(Ruler, Component)
-
-function Ruler() {
-  if (!(this instanceof Ruler)) return new Ruler()
-  Component.call(this)
-  this.width = null
-  this.height = null
-  this.x = null
-  this.y = null
-  this.cellWidth = null
-}
-
-Ruler.prototype._render = function (opts) {
-  const width = this.width = opts.width
-  const height = this.height = opts.height
-  const x = this.x = opts.x || 0
-  const y = this.y = opts.y || 0
-  const cellWidth = this.cellWidth = opts.cellWidth
-  const updated = html`
-    <g transform="translate(${x}, ${y})">
-      <rect width=${width} height=${height - 2} fill="hsl(0, 0%, 30%)" />
+module.exports = () => {
+  const c = component({
+    name: 'svg-daw-ruler',
+    pure: true
+  })
+  c.on('render', () => html`
+    <g transform="translate(${c.props.x || 0}, ${c.props.y || 0})">
+      <rect width=${c.props.width} height=${c.props.height - 2} fill="hsl(0, 0%, 30%)" />
       <g>
-        ${Array(Math.ceil(width / cellWidth)).fill(0).map((_, i) => html`
+        ${Array(Math.ceil(c.props.width / c.props.cellWidth)).fill(0).map((_, i) => html`
           <line
-            x1=${i * cellWidth}
+            x1=${i * c.props.cellWidth}
             y1=${i % 4 === 0
-              ? height / 1.4
-              : height / 1.3}
-            x2=${i * cellWidth}
-            y2=${height - 2}
+              ? c.props.height / 1.4
+              : c.props.height / 1.3}
+            x2=${i * c.props.cellWidth}
+            y2=${c.props.height - 2}
             stroke=${i % 4 === 0
               ? 'hsl(0, 0%, 60%)'
               : 'hsl(0, 0%, 50%)'}
           />
         `)}
       </g>
-      <line x1=0 y1=${height - 2} x2=${width} y2=${height - 2} stroke="black" />
-      <line x1=0 y1=${height - 1} x2=${width} y2=${height - 1} stroke="hsla(0, 0%, 0%, 0.5)" />
-    </g>`
-  if (this._element) morph(this._element, updated)
-  else this._element = updated
-  return this._element
+      <line x1=0 y1=${c.props.height - 2} x2=${c.props.width} y2=${c.props.height - 2} stroke="black" />
+      <line x1=0 y1=${c.props.height - 1} x2=${c.props.width} y2=${c.props.height - 1} stroke="hsla(0, 0%, 0%, 0.5)" />
+    </g>
+  `)
+  return c
 }
-
-Ruler.prototype._update = function (opts) {
-  return opts.width !== this.width
-    || opts.height !== this.height
-    || opts.x !== this.x
-    || opts.y !== this.y
-    || opts.cellWidth !== this.cellWidth
-}
-
